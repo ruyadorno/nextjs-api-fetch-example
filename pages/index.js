848b1b40fa7a2d5a423cc8b3b29067e05a1697b3
Ruy Adorno <ruyadorno@hotmail.com>
2017-07-06 01:30:44 -0400

    Added fetch posts, initial index page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,24 @@
-export default () => (
-  <div>Welcome to next.js!</div>
-)
+import React from 'react';
+import fetch from 'isomorphic-fetch';
+
+export default class extends React.Component {
+    static async getInitialProps() {
+        const data = await fetch('http://localhost:8059/posts');
+        const items = await data.json();
+        return {
+            items
+        };
+    }
+    render() {
+        const { items } = this.props;
+        return (
+            <section>
+                <h1>Node.js Server Side Render in the Age of APIs</h1>
+                <ul>
+                    { items.map(i => <li>{ i.title }</li>) }
+                </ul>
+            </section>
+        );
+    }
+}
 
